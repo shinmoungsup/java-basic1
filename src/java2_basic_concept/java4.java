348889f227a2120d8f3f653bc257f426c1b2bf42
shinmoungsup <noodu0529@gmail.com>
2020-08-19 14:57:45 +0900

    java2.상속,파라미터,아규먼트,리턴

---
+++ src/java2_basic_concept/java4.java
@@ -0,0 +1,17 @@
+package java2_basic_concept;
+
+public class java4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		// argument : 입력을 받는 파라미터라는 뜻.
+    	String text1 = args[0];
+    	String text2 = args[1];
+
+        System.out.println("Hello");
+        System.out.println(text2);
+
+	}
+
+}
---
+++ src/java2_basic_concept/java6.java
@@ -0,0 +1,24 @@
+package java2_basic_concept;
+
+public class java6 {
+
+    public static String a() {
+        // ...
+        return "a";
+    }
+
+    public static int one() {
+        return 1;
+        //...
+    }
+
+    public static void main(String[] args) {
+
+//    	a();
+//    	one();
+        System.out.println(a());
+        System.out.println(one());
+
+    }
+
+}
